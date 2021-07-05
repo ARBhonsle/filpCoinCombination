a79126cfbf179f1bcb7b9f6ac500db4a21984a92
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-06 00:02:18 +0530

    adding filp result in dictionary and give result percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 echo "Flip Coin Combination"
-val=$((RANDOM%2))
-if [ $val -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A output
+output[H]=0
+putput[T]=0
+echo "give no of flips:"
+read n
+for((count=0;count<$n;count++))
+do
+	val=$((RANDOM%2))
+	if [ $val -eq 1 ]
+	then
+		#echo "Heads"
+		((output[H]++))
+	else
+		#echo "Tails"
+		((output[T]++))
+	fi
+done
+percentH=`awk -v h=${output[H]} 'BEGIN {print (h/20)}'`
+percentT=`awk -v t=${output[T]} 'BEGIN {print (t/20)}'`
+echo "No of flips:$n"
+echo "Heads: ${output[H]}, percent:$percentH"
+echo "Tails: ${output[T]}, percent:$percentT"
