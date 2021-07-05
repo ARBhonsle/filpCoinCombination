1fc4a03967fbaa317d2257c37e357a69890f3a4b
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-06 00:51:34 +0530

    added triplet dectionary and percent

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -14,8 +14,18 @@ output[HT]=0
 output[TH]=0
 output[TT]=0
 
+#triplet combination
+output[HHH]=0
+output[HHT]=0
+output[HTH]=0
+output[HTT]=0
+output[THH]=0
+output[THT]=0
+output[TTH]=0
+output[TTT]=0
+
 #type of flip
-echo "singlet:1 doublet:2"
+echo "singlet:1 doublet:2 triplet:3"
 read type
 
 #no of flips
@@ -45,8 +55,8 @@ function singletFlipCoin()
 
 	#printing for singlet
 	echo "No of flips:$n"
-	echo "Heads: ${output[H]}, percent:$percentH"
-	echo "Tails: ${output[T]}, percent:$percentT"
+	echo "H: ${output[H]}, percent:$percentH"
+	echo "T: ${output[T]}, percent:$percentT"
 }
 
 #for doublet combination
@@ -78,17 +88,86 @@ function doubletFlipCoin()
 	percentTH=`awk -v th=${output[TH]} -v n=$n 'BEGIN {print (th/n)}'`
 
 	#printing for doublet
-	echo "Heads: ${output[HH]}, percent: $percentHH"
-	echo "Tails: ${output[TT]}, percent: $percentTT"
-	echo "Heads: ${output[HT]}, percent: $percentHT"
-	echo "Tails: ${output[TH]}, percent: $percentTH"
+	echo "HH: ${output[HH]}, percent: $percentHH"
+	echo "TT: ${output[TT]}, percent: $percentTT"
+	echo "HT: ${output[HT]}, percent: $percentHT"
+	echo "TH: ${output[TH]}, percent: $percentTH"
 }
+
+#for triplet combination
+#for doublet combination
+function tripletFlipCoin()
+{
+	n=$1
+	for((count=0;count<$n;count++))
+	do
+		val1=$((RANDOM%2))
+		val2=$((RANDOM%2))
+		val3=$((RANDOM%2))
+		if [ $val1 -eq 1 -a $val2 -eq 1 ]
+		then
+			if [ $val3 -eq 1 ]
+			then
+				((output[HHH]++))
+			else
+				((output[HHT]++))
+			fi
+		elif [ $val1 -eq 0 -a $val2 -eq 0 ]
+		then
+			if [ $val3 -eq 1 ]
+			then
+				((output[TTH]++))
+			else
+				((output[TTT]++))
+			fi
+		elif [ $val1 -eq 1 -a $val2 -eq 0 ]
+		then
+			if [ $val3 -eq 1 ]
+			then
+				((output[HTH]++))
+			else
+				((output[HTT]++))
+			fi
+		else
+			if [ $val3 -eq 1 ]
+			then
+				((output[THH]++))
+			else
+				((output[THT]++))
+			fi
+		fi
+	done
+
+	#output percent calculation
+	percentHHH=`awk -v hhh=${output[HHH]} -v n=$n 'BEGIN {print (hhh/n)}'`
+	percentTTH=`awk -v tth=${output[TTH]} -v n=$n 'BEGIN {print (tth/n)}'`
+	percentHTH=`awk -v hth=${output[HTH]} -v n=$n 'BEGIN {print (hth/n)}'`
+	percentTHH=`awk -v thh=${output[THH]} -v n=$n 'BEGIN {print (thh/n)}'`
+	percentHHT=`awk -v hht=${output[HHT]} -v n=$n 'BEGIN {print (hht/n)}'`
+	percentTTT=`awk -v ttt=${output[TTT]} -v n=$n 'BEGIN {print (ttt/n)}'`
+	percentHTT=`awk -v htt=${output[HTT]} -v n=$n 'BEGIN {print (htt/n)}'`
+	percentTHT=`awk -v tht=${output[THT]} -v n=$n 'BEGIN {print (tht/n)}'`
+
+	#printing for doublet
+	echo "HHH: ${output[HHH]}, percent: $percentHHH"
+	echo "TTH: ${output[TTH]}, percent: $percentTTH"
+	echo "HTH: ${output[HTH]}, percent: $percentHTH"
+	echo "THH: ${output[THH]}, percent: $percentTHH"
+	echo "HHT: ${output[HHT]}, percent: $percentHHT"
+	echo "TTT: ${output[TTT]}, percent: $percentTTT"
+	echo "HTT: ${output[HTT]}, percent: $percentHTT"
+	echo "THT: ${output[THT]}, percent: $percentTHT"
+}
+
 if [ $type -eq 2 ]
 then
 	doubletFlipCoin $n
 elif [ $type -eq 1 ]
 then
 	singletFlipCoin $n
+elif [ $type -eq 3 ]
+then
+	tripletFlipCoin $n
 else
 	echo "wrong choice"
 fi
